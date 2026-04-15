1fbd99d0dbeb21e7ba41fcbce709ba6a21b88a21
Jignesh <pithvajignesh@gmail.com>
2026-04-15 12:44:56 +0530

    Removed docker-compose and run script

--- run.ps1
+++
@@ -1,8 +0,0 @@
-# Stop & remove old container
-docker rm -f $(docker ps -aq --filter "ancestor=jemena-camunda-poc")
-
-# Build latest image
-docker build -t jemena-camunda-poc .
-
-# Run container using env file
-docker run --env-file .env -p 8080:8080 jemena-camunda-poc
